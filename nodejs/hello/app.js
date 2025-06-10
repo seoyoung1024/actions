8dfa705cc33d6c28125457a49ef5a517ecbaabd1
seoyoung1024 <jjssyy0608@gmail.com>
2025-06-10 07:22:04 +0000

    up

--- nodejs/hello/app.js
+++ nodejs/hello/app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-    res.send("Hello World - EC2 Deploy!");
+    res.send("Hello World - EC2 Deploy!!");
 });
 
 app.listen(8000, function () {
